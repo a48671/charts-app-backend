11a412587227d7277059bf34c5788a64ebe2ad3f
Фомин Андрей Александрович <fomin_a@tochka.com>
2021-07-04 17:17:53 +0500

    update data

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const PORT = process.env.PORT || 8080;
-const temperature = require('./db/temperature.js');
-const precipitation = require('./db/precipitation.js');
+const temperature = require('./db/temperature.json');
+const precipitation = require('./db/precipitation.json');
 const { getDataByDateRange } = require('./utils');
 
 const app = express();
